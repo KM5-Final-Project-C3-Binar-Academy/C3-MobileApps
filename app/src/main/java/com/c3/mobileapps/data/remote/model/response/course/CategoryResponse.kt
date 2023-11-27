c748ad16b08884e907c11a5a3f6705287661c711
Ardanudn <danuduhri28@gmail.com>
2023-11-27 20:02:46 +0700

    Hompepage showing data

---
+++ app/src/main/java/com/c3/mobileapps/data/remote/model/response/course/CategoryResponse.kt
@@ -0,0 +1,9 @@
+package com.c3.mobileapps.data.remote.model.response.course
+
+
+import com.google.gson.annotations.SerializedName
+
+data class CategoryResponse(
+    @SerializedName("data")
+    var `data`: List<Category?>?
+)
