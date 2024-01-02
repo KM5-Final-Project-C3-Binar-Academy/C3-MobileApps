657841e966ea98d5ca32cf6389e958ffeafa7a51
E41212006 Mochammad Enrique Lazuardi Ramadany <e41212006@student.polije.ac.id>
2024-01-02 19:07:04 +0700

    Merging and Handle Conflict Branch

--- app/src/main/java/com/c3/mobileapps/ui/otp/otpFragment.kt
+++ app/src/main/java/com/c3/mobileapps/ui/otp/otpFragment.kt
@@ -1,5 +1,6 @@
 package com.c3.mobileapps.ui.otp
 
+import android.app.Activity
 import android.content.Context
 import android.os.Bundle
 import android.text.Editable
@@ -9,19 +10,24 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.c3.mobileapps.R
+import com.c3.mobileapps.data.local.SharedPref
 import com.c3.mobileapps.data.remote.model.request.auth.OtpRequest
 import com.c3.mobileapps.data.remote.model.request.auth.RegisterRequest
 import com.c3.mobileapps.databinding.FragmentOtpBinding
+import com.c3.mobileapps.ui.login.LoginViewModel
 import com.c3.mobileapps.utils.CustomSnackbar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import org.koin.android.ext.android.inject
 
 class OtpFragment : Fragment() {
 	private lateinit var binding: FragmentOtpBinding
+	private val loginViewModel: LoginViewModel by inject()
 	private val otpViewModel: OtpViewModel by inject()
+	private val sharedPreferences: SharedPref by inject()
 	private val snackbar = CustomSnackbar() // Custom Snackbar Object Class
 
 	override fun onCreateView(
@@ -38,7 +44,7 @@ class OtpFragment : Fragment() {
 
 		val registerData: RegisterRequest? = arguments?.getParcelable("RegisterModel")
 		val email   = registerData?.email.toString()
-		val nama    = registerData?.name.toString()
+		val pass    = registerData?.password.toString()
 
 		// Observe Text Length
 		binding.etOTP.addTextChangedListener(object : TextWatcher {
@@ -50,10 +56,10 @@ class OtpFragment : Fragment() {
 				val kodeOTP = binding.etOTP.text
 
 				if(kodeOTP.length == 6){
-					binding.constraintOTP.visibility = View.VISIBLE
+					// Hilangkan keyboard
+					hideKeyboard()
 
-					Log.i("OTP Issues", email)
-					Log.i("OTP Issues", kodeOTP.toString())
+					binding.constraintOTP.visibility = View.VISIBLE
 
 					// Wrap into Dataclass
 					val modelData = OtpRequest(email,kodeOTP.toString())
@@ -62,22 +68,50 @@ class OtpFragment : Fragment() {
 					otpViewModel.otpResponse.observe(viewLifecycleOwner, Observer {res ->
 						when (res.code()) {
 							200 -> {
-								snackbar.showSnackbarUtils("Verifikasi Berhasil! Selamat Datang, $nama", false, layoutInflater, requireView(), requireContext())
-								// Intent to Loginpage
-								findNavController().navigate(R.id.homeFragment)
+								// Notify the user
+								snackbar.showSnackbarUtils("Verifikasi Berhasil! Sedang Mengarahkanmu ke Beranda..", false, layoutInflater, requireView(), requireContext())
+
+								// Logging new User
+								loginViewModel.login(email,pass)
+
+								// Handle Login Response
+								loginViewModel.loginResponse.observe(viewLifecycleOwner, Observer { res ->
+
+									Log.d("Login Response", res.code().toString())
+
+									when (res.code()){
+										200 -> {
+											snackbar.showSnackbarUtils("Login Berhasil!", false, layoutInflater, requireView(), requireContext())
+
+											val data = res.body()?.data
+
+											sharedPreferences.setIsLogin(true)
+											sharedPreferences.setToken("Bearer ${data?.token}")
+
+											// Intent to Homepage
+											findNavController().navigate(R.id.homeFragment)
+										}
+
+										500 -> {
+											snackbar.showSnackbarUtils("Aplikasi dalam perbaikan. Mohon Coba Lagi!", true, layoutInflater, requireView(), requireContext())
+										}
+									}
+								})
+
 							}
 
-							400 -> {
-								snackbar.showSnackbarUtils("Validasi Error", true, layoutInflater, requireView(), requireContext())
+							401 -> {
+								snackbar.showSnackbarUtils("Kode OTP Tidak Cocok", true, layoutInflater, requireView(), requireContext())
 							}
 
 							500 -> {
 								snackbar.showSnackbarUtils("Aplikasi dalam perbaikan. Mohon Coba Lagi", true, layoutInflater, requireView(), requireContext())
 							}
 						}
-					})
 
-					binding.constraintOTP.visibility = View.INVISIBLE
+						// Finish Loading
+						binding.constraintOTP.visibility = View.INVISIBLE
+					})
 				}
 			}
 
@@ -86,6 +120,15 @@ class OtpFragment : Fragment() {
 			}
 
 		})
+	}
+
+	private fun Fragment.hideKeyboard() {
+		view?.let { activity?.hideKeyboard(it) }
+	}
+
+	private fun Context.hideKeyboard(view: View) {
+		val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+		inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
 	}
 
 	override fun onAttach(context: Context) {
